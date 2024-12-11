2796c65f13103c7a3953fd90f165964f5aebacd6
igor.kainara <igor.kainara@gmail.com>
2024-12-11 18:02:14 +0100

    add Allure

--- src/main/java/org/ikainara/orangehrm_at/annotations/OrangeTest.java
+++ src/main/java/org/ikainara/orangehrm_at/annotations/OrangeTest.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@ExtendWith(PageParameterResolver.class)
+@ExtendWith({PageParameterResolver.class})
 @Test
 public @interface OrangeTest {
 }
